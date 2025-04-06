fe377a3ea5a437935ed42c08750dd9c282b75a63
kirhtarg <kirhtarg@gmail.com>
2025-04-06 17:00:01 +0300

    events update

--- app/Http/Controllers/Api/ApiEventController.php
+++ app/Http/Controllers/Api/ApiEventController.php
@@ -45,7 +45,7 @@ class ApiEventController extends Controller
 
         // Основной запрос с фильтрацией
         $query = Event::query()
-            ->select('id', 'title', 'date_from', 'date_to', 'result', 'image', 'competition_id', 'arena_id', 'club1_id', 'club2_id', 'event_name')
+            ->select('id', 'title', 'date_from', 'date_to', 'result', 'result_dop', 'image', 'competition_id', 'arena_id', 'club1_id', 'club2_id', 'event_name')
             ->with([
                 'competition' => function ($query) {
                     $query->select([
