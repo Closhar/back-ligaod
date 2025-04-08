afe1d4570fe7cef8e1b162378de7a4d65905e804
kirhtarg <kirhtarg@gmail.com>
2025-04-09 00:35:20 +0300

    events update

--- routes/api.php
+++ routes/api.php
@@ -155,7 +155,7 @@ Route::post('/upload-image', function(Request $request) {
         $fileName = Str::uuid() . '.' . $file->getClientOriginalExtension();
 
         // Сохранение с указанием явного пути
-        $path = $file->storeAs('public/editor-images/' . date('Y/m/d'), $fileName);
+        $path = $file->storeAs('public/images/' . date('Y/m/d'), $fileName);
 
         // Генерация URL без использования asset() для API
         $url = Storage::url($path);
