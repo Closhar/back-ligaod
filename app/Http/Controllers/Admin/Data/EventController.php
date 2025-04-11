0a50fc1b872c39f79a8589872023b12264495993
kirhtarg <kirhtarg@gmail.com>
2025-04-11 08:32:22 +0300

    events api

--- app/Http/Controllers/Admin/Data/EventController.php
+++ app/Http/Controllers/Admin/Data/EventController.php
@@ -370,7 +370,7 @@ class EventController extends Controller
     {
         try {
             $validated = $request->validate([
-                'title' => 'string|max:255',
+                'title' => 'string|max:255|nullable',
                 'result' => 'string|max:255|nullable',
                 'result_dop' => 'string|max:255|nullable',
                 'date_from' => 'sometimes',
