850aeb51561f3939b6d53fd7cd0a30d59df7e16e
kirhtarg <kirhtarg@gmail.com>
2025-04-20 12:49:47 +0300

    sport error

--- app/Http/Controllers/Api/ApiEventController.php
+++ app/Http/Controllers/Api/ApiEventController.php
@@ -267,11 +267,6 @@ class ApiEventController extends Controller
 
         // преобразуем дату и время
         $events->transform(function ($event) {
-            if ($event->competition && $event->competition->sport) {
-                $event->sport_title = $event->competition->sport->title;
-            } else {
-                $event->sport_title = null;
-            }
             // Преобразуем date_from в формат "DD.MM.YYYY."
             $event->date_formatted = \Carbon\Carbon::parse($event->date_from)->format('d.m.Y.');
 
