44bf404a365bf637ff8417ce1824dae0bec28f8b
kirhtarg <kirhtarg@gmail.com>
2025-04-22 12:34:02 +0300

    city_title

--- app/Http/Controllers/Admin/Data/ClubController.php
+++ app/Http/Controllers/Admin/Data/ClubController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\ApiGenderRequest;
 use App\Models\Age;
 use App\Models\Club;
+use App\Models\Event;
 use App\Models\Gender;
 use App\Models\Sport;
 use Illuminate\Http\Request;
