ee08e0d8f654e120843fef6a97240b4909f73dc3
kirhtarg <kirhtarg@gmail.com>
2025-05-03 16:22:49 +0300

    in_profile

--- app/Http/Controllers/Admin/Data/EventController.php
+++ app/Http/Controllers/Admin/Data/EventController.php
@@ -69,7 +69,7 @@ class EventController extends Controller
         // Основной запрос с фильтрацией
         $query = Event::query()
             ->select('id', 'region_id', 'series_id', 'title', 'date_from', 'date_to', 'result', 'result_dop', 'image', 'competition_id', 'arena_id',
-                'club1_id', 'club2_id', 'event_name', "is_active", 'about', 'series_count2',
+                'club1_id', 'club2_id', 'event_name', "is_active", 'about', 'series_count',
                 DB::raw('CONCAT("' . config('app.url') . '", "/storage/", events.image) AS event_image_path')
             )
             ->withCount('streams')
