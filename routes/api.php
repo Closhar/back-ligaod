ab2382838583ea85dda812444d9d7f6b41459e66
kirhtarg <kirhtarg@gmail.com>
2025-05-31 11:11:02 +0300

    events competitions

--- routes/api.php
+++ routes/api.php
@@ -134,9 +134,9 @@ Route::apiResource('admin-pages', AdminPageController::class);
 
     // Маршруты для работы с телеграм
     Route::prefix('telegram')->group(function () {
-        Route::get('/channels', [TelegramController::class, 'getChannels']);
-        Route::post('/channels', [TelegramController::class, 'create']);
-        Route::put('/channels/{id}', [TelegramController::class, 'update']);
+        Route::apiResource('channels', TelegramController::class);
+        // Route::post('/channels', [TelegramController::class, 'create']);
+        // Route::put('/channels/{id}', [TelegramController::class, 'update']);
         Route::post('/send-message', [TelegramController::class, 'sendMessage']);
     });
 
