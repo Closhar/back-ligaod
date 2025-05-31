8dc77677f333d9a8507fe4dfe368b69b08a07648
kirhtarg <kirhtarg@gmail.com>
2025-05-31 08:33:04 +0300

    events competitions

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -131,7 +131,7 @@ class Event extends Model
     public function getEventNameAttribute()
     {
         // Добавляем параметр event_name_last
-        if (!$this->club1) return $this->competition?->title . '. ' . $this->title;
+        if (!$this->club1) return $this->competition?->title_short . '. ' . $this->title;
         else
             return $this->date_formatted . ' ' . ($this->club1->title ?? 'Клуб 1') . ' (' . ($this->club1->city->title ?? 'Город не указан') . ') - ' . ($this->club2->title ?? 'Клуб 2') . ' (' . ($this->club2->city->title ?? 'Город не указан') . ') ' . $this->result;
 
@@ -140,7 +140,7 @@ class Event extends Model
     public function getEventNameTopAttribute()
     {
         // Добавляем параметр event_name_last
-        if (!$this->club1) return $this->competition?->title . '. ' . $this->title;
+        if (!$this->club1) return $this->competition?->title_short . '. ' . $this->title;
         else
             return ($this->club1->title ?? 'Клуб 1') . ' (' . ($this->club1->city->title ?? 'Город не указан') . ') - ' . ($this->club2->title ?? 'Клуб 2') . ' (' . ($this->club2->city->title ?? 'Город не указан') . ') ';
 
