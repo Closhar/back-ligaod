ee69b46881ec629925e501c0f6440b90a6f1369b
kirhtarg <kirhtarg@gmail.com>
2025-06-01 21:23:04 +0300

    event-region

--- app/Http/Controllers/Admin/Data/EventController.php
+++ app/Http/Controllers/Admin/Data/EventController.php
@@ -211,7 +211,14 @@ class EventController extends Controller
                                   ->orWhereNull('region_id');
                         });
                     });
-                });
+                })
+                ->join('clubs as club1', 'events.club1_id', '=', 'club1.id')
+                ->join('clubs as club2', 'events.club2_id', '=', 'club2.id')
+                ->where(function($q) use ($regionId) {
+                    $q->where('club1.region_id', $regionId)
+                      ->orWhere('club2.region_id', $regionId);
+                })
+                ->select('events.*');
             }
 
             if ($with_team) {
