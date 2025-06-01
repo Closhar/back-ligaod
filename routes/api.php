da700933114f0492b9b2c8b03344a548330c794d
kirhtarg <kirhtarg@gmail.com>
2025-06-01 23:52:41 +0300

    chat-gpt

--- routes/api.php
+++ routes/api.php
@@ -57,8 +57,8 @@ Route::group(['prefix' => '/v1', 'namespace' => 'Api'], function () {
     Route::get('/ages/{id}', [ApiAgeController::class, 'show'])->name('ages.show');
     Route::get('/sport_properties', [ApiSportPropertyController::class, 'index'])->name('sport_properties.index');
     Route::get('/sport_properties/{id}', [ApiSportPropertyController::class, 'show'])->name('sport_properties.show');
-    Route::get('/sports', [ApiSportController::class, 'index'])->name('sports.index');
-    Route::get('/sports/{id}', [ApiSportController::class, 'show'])->name('sports.show');
+    Route::get('/sports', [ApiSportController::class, 'index'])->name('api.sports.index');
+    Route::get('/sports/{id}', [ApiSportController::class, 'show'])->name('api.sports.show');
     Route::get('/cities', [ApiCityController::class, 'index'])->name('cities.index');
     Route::get('/cities/{id}', [ApiCityController::class, 'show'])->name('cities.show');
     Route::get('/clubs', [ApiClubController::class, 'index'])->name('clubs.table');
