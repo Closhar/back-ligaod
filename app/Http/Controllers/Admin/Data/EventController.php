8e085487d1bafa8c1f342b82dd085309cdb03c69
kirhtarg <kirhtarg@gmail.com>
2025-06-01 21:27:07 +0300

    event-region

--- app/Http/Controllers/Admin/Data/EventController.php
+++ app/Http/Controllers/Admin/Data/EventController.php
@@ -218,8 +218,6 @@ class EventController extends Controller
                     $q->where('club1.region_id', $regionId)
                       ->orWhere('club2.region_id', $regionId);
                 })
-                ->where('events.region_id', $regionId)
-                ->where('events.is_active', 1)
                 ->select('events.*');
             }
 
