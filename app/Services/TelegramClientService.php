8d2f3f00eeb503a5dcb967dac3b93417781ab536
kirhtarg <kirhtarg@gmail.com>
2025-06-09 00:19:22 +0300

    telegram parse

--- app/Services/TelegramClientService.php
+++ app/Services/TelegramClientService.php
@@ -140,19 +140,19 @@ class TelegramClientService
             $self = $this->madelineProto->getSelf();
             \Log::info('Информация о текущем пользователе:', ['self' => $self]);
 
-            // Получаем полную информацию о канале
-            $fullInfo = $this->madelineProto->getFullInfo($channel->channel_id);
-            \Log::info('Получена полная информация о канале:', ['fullInfo' => $fullInfo]);
+            // Получаем информацию о канале через getPwrChat
+            $pwrChat = $this->madelineProto->getPwrChat($channel->channel_id);
+            \Log::info('Получена информация через getPwrChat:', ['pwrChat' => $pwrChat]);
 
-            if (!isset($fullInfo['Chat']) || !isset($fullInfo['Chat']['id']) || !isset($fullInfo['Chat']['access_hash'])) {
-                throw new \Exception('Не удалось получить полную информацию о канале');
+            if (!isset($pwrChat['id']) || !isset($pwrChat['type']) || $pwrChat['type'] !== 'channel') {
+                throw new \Exception('Не удалось получить информацию о канале');
             }
 
             // Формируем InputPeerChannel
             $inputPeer = [
                 '_' => 'inputPeerChannel',
-                'channel_id' => abs($fullInfo['Chat']['id']),
-                'access_hash' => $fullInfo['Chat']['access_hash']
+                'channel_id' => abs($pwrChat['id']),
+                'access_hash' => $pwrChat['access_hash'] ?? 0
             ];
 
             \Log::info('Сформирован InputPeerChannel:', ['inputPeer' => $inputPeer]);
